64e02f84539e4914494d08d9631342d47135056d
Rinor Sadiku <rinor.sadiku2@gmail.com>
2019-03-14 06:11:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+// Generate the express app
+const app = express();
+
+// Setting up a route handler
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' });
+});
+
+// Telling express on which port to listen for requests
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
